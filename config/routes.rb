2006b95eae62477829dddde9daac940c5d81c970
Doug Barr <douglas.barr01@gmail.com>
2023-03-02 14:03:26 +0000

    Reviews route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
  root to: "restaurants#index"
   resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:new, :create]
   end
 end
